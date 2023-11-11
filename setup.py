91859b500e85ca31ccfa3a7673bde22a6df09a2d
francisco-rai <francisco.mendes.pv@renesas.com>
2023-11-11 17:16:27 -0600

    fixed errors

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyHopperFeatures',
-    version='0.0.4',
+    version='0.0.5',
     author="francisco-rai",
     author_email="francisco.mendes.pv@renesas.com",
     packages=find_packages(where="src"),
